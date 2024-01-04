7a24eaae9f11ace735b633f0f4d5994338cd6cc5
Alandis <1seals.alandis@gmail.com>
2024-01-04 11:45:36 -0600

    Add docstring

---
+++ RickAndMorty/APIClients/RMEndpoint.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+
+/// Reprent unique API endpoint
+@frozen enum Endpiont: String {
+    ///API Endpoints
+    case episode
+    case character
+    case location
+}
---
+++ RickAndMorty/APIClients/RMRequest.swift
@@ -0,0 +1,14 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Alandis Seals on 1/4/24.
+//
+
+import Foundation
+
+
+/// Object that represent a single object call 
+final class RMRequest {
+    
+}
