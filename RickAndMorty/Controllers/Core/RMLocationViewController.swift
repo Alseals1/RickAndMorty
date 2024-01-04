b97431c30b9e103a6c3088ff646daa81d3bc7f74
Alandis <1seals.alandis@gmail.com>
2024-01-04 11:01:10 -0600

    Added tabbar and Navigation title

---
+++ RickAndMorty/Controllers/Core/RMLocationViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+final class RMLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Location"
+    }
+
+
+}
---
+++ RickAndMorty/Controllers/Core/RMSettingViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+final class RMSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+
+        title = "Setting"
+    }
+
+}
